53dd21b21e9de22e84eddcc1118969b2c2c9f38e
Iteration Increment <ehakdrktj@gmail.com>
2017-11-16 12:33:33 +0900

    Add files via upload
    Add Filter.cs
     - Create a filtering source class
     - Create a form to view the results of filtering

---
+++ PhotoAnalysis/PhotoAnalysis/FilterImageForm.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace PhotoAnalysis
+{
+    public partial class FilterImageForm : Form
+    {
+        public FilterImageForm()
+        {
+            InitializeComponent();
+        }
+
+        private void pictureBox1_Click(object sender, EventArgs e)
+        {
+            SaveFileDialog fd = new SaveFileDialog();
+
+            fd.FileName = System.DateTime.Now.ToString("yyyyMMddHHmmss");
+            fd.DefaultExt = "jpg";
+            fd.Filter = "JPEG files (*.jpg)|*.jpg|All files (*.*)|*.*";
+
+            if (fd.ShowDialog() == DialogResult.OK)
+            {
+                pictureBox1.Image.Save(fd.FileName);
+            }  
+        }
+    }
+}
