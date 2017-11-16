60b3cf5f410f4eff6d01bb675fcedb97d035c9a0
hyunil-stdlib <ehakdrktj@gmail.com>
2017-11-16 12:58:12 +0900

    Add files via upload
    Add Filter.cs
     - Create a filtering source class
     - Create a form to view the results of filtering

--- PhotoAnalysis/Program.cs
+++ PhotoAnalysis/Program.cs
@@ -16,7 +16,7 @@ namespace PhotoAnalysis
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new Form1());
+            Application.Run(new MainForm());
         }
     }
 }
